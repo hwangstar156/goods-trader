4f4eb971c2da06866894bb40d174d5a93c7338de
hwangstar156 <85891751+hwangstar156@users.noreply.github.com>
2021-10-25 23:20:08 +0900

    rename menu-container folder and delete console.log

--- .history/src/redux/root-saga_20210916222440.js
+++ .history/src/redux/root-saga_20210916222440.js
@@ -1,7 +1,6 @@
 import { all, call } from "redux-saga/effects";
 import { userSagas } from "./user/user.saga";
 import { writingSagas } from "./writings/writings.saga";
-
 export default function* rootSaga() {
   yield all([call(userSagas), call(writingSagas)]);
 }
---
+++ src/component/menu-container/menu-container.component.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { useSelector } from "react-redux";
+
+import MenuItem from "../menu-item/menu-item.component";
+import { selectIdols } from "../../redux/idol/idol.selector";
+import "./menu-container.styles.scss";
+import MenuAside from "../menu-aside/menu-aside.component";
+
+const MenuContainer = () => {
+  const sections = useSelector(selectIdols);
+
+  return (
+    <div className="menu-container">
+      <div className="menu-items">
+        {sections.map(({ id, ...otherProps }) => (
+          <MenuItem key={id} {...otherProps} />
+        ))}
+      </div>
+      <MenuAside />
+    </div>
+  );
+};
+
+export default MenuContainer;
